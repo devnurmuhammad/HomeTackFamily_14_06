d58b705120f62c101c3495c0d8b231203bc4dfb7
devnurmuhammad <nurmuhammad1557@gmail.com>
2023-06-16 16:23:53 +0500

    Add project files.

---
+++ HomeTackFamily_14_06/Family.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeTackFamily_14_06;
+
+public class Person
+{
+    public bool oilalimi = false;
+    public string Ism { get; set; }
+    public Person jufti { get; set; }
+
+    public static void FHDYO(Person p1,  Person p2)
+    {
+        p1.oilalimi =true;
+        p2.oilalimi =true;
+    }
+    void func2(Person p) { 
+        p.oilalimi =true;
+    }
+}
---
+++ HomeTackFamily_14_06/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeTackFamily_14_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+            person1.Ism = "Sanjarbek";
+
+
+            Person.FHDYO(person1, person2);
+            Console.WriteLine(person1.Ism + " " + person1.oilalimi);
+
+        }
+        
+    }
+}
